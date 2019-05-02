dfaa9baba39d467e5aa0f84428287726ebcd93d3
Kathleen Montesano <kathleen.m.montesano@gmail.com>
2019-05-02 15:04:13 -0400

    starting logic

--- Friendfinder/app/data/friends.js
+++ Friendfinder/app/data/friends.js
@@ -1,7 +1,126 @@
 var friendsArray = [
     {
-        friendName: "kathleen"
-    }
+        "name": "Ahmir",
+        "photo": "https://cdn.stocksnap.io/img-thumbs/960w/DVXSK4K9ZV.jpg",
+        "scores": [
+            4,
+            3,
+            5,
+            1,
+            4,
+            2,
+            2,
+            2,
+            2,
+            3
+        ]
+    }, {
+        "name": "Jen",
+        "photo": "https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/k-s21-ae-5266.jpg?w=1400&dpr=1&fit=default&crop=default&auto=format&fm=jpg&q=75&vib=3&con=3&usm=15&markpad=13&markalpha=90&markscale=10&markx=25&mark=rawpixel-watermark.png&ixlib=js-1.1.1&s=2a5d85a9efc13970f7049365224ad32a",
+        "scores": [
+            2,
+            2,
+            3,
+            4,
+            3,
+            1,
+            5,
+            4,
+            4,
+            1
+        ]
+    }, {
+        "name": "Glen",
+        "photo": "https://assets3.thrillist.com/v1/image/1299823/size/tmg-article_default_mobile.jpg",
+        "scores": [
+            1,
+            3,
+            4,
+            3,
+            3,
+            3,
+            2,
+            4,
+            2,
+            4
+        ]
+    }, {
+        "name": "Ashley",
+        "photo": "https://image.shutterstock.com/mosaic_250/2936380/613759379/stock-photo-happy-cheerful-young-woman-wearing-her-red-hair-in-bun-rejoicing-at-positive-news-or-birthday-gift-613759379.jpg",
+        "scores": [
+            2,
+            5,
+            3,
+            2,
+            1,
+            1,
+            5,
+            4,
+            3,
+            3
+        ]
+    }, {
+        "name": "Van",
+        "photo": "https://www.freegreatpicture.com/files/94/28311-business-people-stock.jpg",
+        "scores": [
+            3,
+            2,
+            5,
+            4,
+            3,
+            4,
+            1,
+            3,
+            2,
+            1
+        ]
+    }, {
+        "name": "Rita",
+        "photo": "https://static1.squarespace.com/static/5761b510356fb07cfd0a7201/5b58a3d96d2a73ba73adf176/5b58a7e503ce64b11fa62a36/1532536811946/red+hair.jpg?format=2500w",
+        "scores": [
+            4,
+            1,
+            2,
+            2,
+            4,
+            5,
+            2,
+            2,
+            4,
+            2
+        ]
+    }, {
+        "name": "Chad and Brad",
+        "photo": "https://img.buzzfeed.com/buzzfeed-static/static/2014-05/enhanced/webdr08/12/11/enhanced-buzz-10059-1399908970-29.jpg?downsize=800:*&output-format=auto&output-quality=auto",
+        "scores": [
+            1,
+            2,
+            4,
+            3,
+            3,
+            2,
+            1,
+            4,
+            5,
+            3
+        ]
+    }, {
+        "name": "Ellie",
+        "photo": "https://ak0.picdn.net/shutterstock/videos/12764780/thumb/11.jpg",
+        "scores": [
+            3,
+            4,
+            2,
+            5,
+            3,
+            2,
+            5,
+            2,
+            3,
+            2
+        ]
+    },
+
 ]
 
 module.exports = friendsArray;
--- Friendfinder/app/routing/apiRoutes.js
+++ Friendfinder/app/routing/apiRoutes.js
@@ -1,9 +1,59 @@
 var friendsData = require('../data/friends.js');
 
 module.exports = function (app) {
-    
+
+    var newUserScoreInt = [];
 
     app.get('/api/friends', function (req, res) {
         res.json(friendsData)
     });
+
+    app.post('/api/friends', function (req, res) {
+        newUserScoreInt = [];
+        friendsData.push(req.body);
+        var newUser = friendsData.pop();
+        var newUserScore = newUser.scores;
+        for (var i = 0; i < newUserScore.length; i++) {
+            var newNumber = parseInt(newUserScore[i]);
+            newUserScoreInt.push(newNumber);
+        }
+        var friendsScoresArray = []
+
+        for (var i = 0; i < friendsData.length; i++) {
+            console.log(friendsData[i].scores)
+              friendsScoresArray.push(friendsData[i].scores)  
+            }
+           
+           
+         for (var i = 0; i < friendsScoresArray.length; i++)   {
+            var difference = []
+            var totalDifference = []
+                for(var j = 0; j < friendsScoresArray[i].length; j++){
+                    
+                    difference.push(Math.abs(friendsScoresArray[i][j] - newUserScoreInt[j]))
+             
+                    
+               
+                   
+                }
+                console.log("_______________________________________________________")
+                for (var k = 0 ; k < difference.length; k++) {
+                    var total = difference + difference[k];
+                }
+                console.log(total)
+                
+               
+         } 
+            
+      
+       
+        
+        console.log("new User " + newUserScoreInt);
+        
+    });
+
+    
+    // for (var i = 0; i < friendsData.length; i++) {
+    //     console.log(friendsData[i].scores)
+    // }
 }
